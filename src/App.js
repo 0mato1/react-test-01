6285df8956533dd8cd32dee581e28194197fff05
0mato1 <1013397509@qq.com>
2021-08-12 17:15:54 +0800

    feature

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@
 /*
  * @Author: your name
  * @Date: 2021-08-05 16:13:05
- * @LastEditTime: 2021-08-12 14:48:24
+ * @LastEditTime: 2021-08-12 17:12:57
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \my-app-01\src\App.js
  */
 import React, { Component } from 'react'
 import Count from './containers/Count'
-import store from './redux/store'
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Count store={store}></Count>
+                <Count></Count>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-06 09:28:09
- * @LastEditTime: 2021-08-06 16:41:10
+ * @LastEditTime: 2021-08-12 17:12:41
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \my-app-01\src\index.js
@@ -9,9 +9,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {Provider} from 'react-redux'
 import store from './redux/store'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
-store.subscribe(() => {
-    ReactDOM.render(<App/>,document.getElementById('root'))
-})
+ReactDOM.render(
+<Provider store={store}>
+    <App/>
+</Provider>        
+,document.getElementById('root'))
